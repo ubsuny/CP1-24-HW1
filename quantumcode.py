c4227f724b819a7a653191650b42dfa3ad63b73a
zbpetersbuf <zbpeters@buffalo.edu>
2024-09-20 11:27:21 -0400

    quantumcode.py
    started first quantum code that maps classical to quantum

---
+++ quantumcode.py
@@ -0,0 +1,9 @@
+theta = Parameter("$\\theta$")
+
+chsh_circuit = QuantumCircuit(2)
+chsh_circuit.h(0)
+chsh_circuit.cx(0, 1)
+chsh_circuit.ry(theta, 0)
+chsh_circuit.draw(output="mpl", idle_wires=False, style="iqp")
+
+#i copied this from https://learning.quantum.ibm.com/tutorial/chsh-inequality
