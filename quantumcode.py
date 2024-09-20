4ba9915e468bfcda825a706615acebef91ae3c4a
zbpetersbuf <zbpeters@buffalo.edu>
2024-09-20 11:41:33 -0400

    Update quantumcode(witheomeinlinecomments).py
    in progress

--- quantumcode.py
+++ quantumcode.py
@@ -1,6 +1,7 @@
 theta = Parameter("$\\theta$")
-
+# this initializes thequantum circut 'same link
 chsh_circuit = QuantumCircuit(2)
+#adds the hadamard gate
 chsh_circuit.h(0)
 chsh_circuit.cx(0, 1)
 chsh_circuit.ry(theta, 0)
