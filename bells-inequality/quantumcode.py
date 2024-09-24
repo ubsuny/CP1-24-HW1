d57abfc2f11089d8fda1c3292d63182d90008675
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-09-24 14:04:17 +0000

    Added the needed preliminary imports for the project (mainly, numpy and qiskit)

--- bells-inequality/quantumcode.py
+++ bells-inequality/quantumcode.py
@@ -24,6 +24,22 @@ All code sourced from https://learning.quantum.ibm.com/tutorial/chsh-inequality 
 
 
 
+# Setting up for general important imports needed for the project
+# importing numpy library to better deal with list-like structures in a more performing way
+import numpy as np
+
+# Qiskit imports
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import SparsePauliOp
+
+# Qiskit Runtime imports
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+
+# Plotting routines
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
 
 
 
